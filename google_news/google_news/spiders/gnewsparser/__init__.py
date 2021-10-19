590fc42557097a177ec2479539c0e49bd28b4895
Jakub Hlavačka <43497822+JKBGIT1@users.noreply.github.com>
2021-10-19 16:53:19 +0200

    Initial commit

---
+++ google_news/google_news/spiders/gnewsparser/__init__.py
@@ -0,0 +1,82 @@
+import feedparser
+from datetime import datetime, timedelta
+import json
+
+
+class GnewsParser:
+    __BASE_URL = "https://news.google.com/rss/search?q=<QUERY><DATERANGE><LOCALE>"
+    __DATE_RANGE = "+after:<AFTER>+before:<BEFORE>"
+    __EN_US_LOCALE = "&ceid=US:en&hl=en-US&gl=US"
+    __SK_SK_LOCALE = "&ceid=SK:en&hl=sk-SK&gl=SK"
+
+    def __init__(self):
+        self.__url = GnewsParser.__BASE_URL
+        self.__last_used_url = self.__url
+        self.__start_date = None
+        self.__end_date = None
+        self.__current_window = None
+        self.__locale = None
+        self.__query = None
+        self.__days_step = 1
+
+    def setup_search(self, query, from_date, to_date, days_step=1, locale="en-us"):
+        self.__setup_base_url(query, locale)
+        self.__start_date = datetime.strptime(from_date, "%Y-%m-%d")
+        self.__end_date = datetime.strptime(to_date, "%Y-%m-%d")
+        self.__current_window = self.__start_date
+        self.__days_step = days_step
+        self.__locale = locale
+        self.__query = query
+
+    def __setup_base_url(self, query, locale):
+        self.__url = self.__url.replace("<QUERY>", self.__get_clean_query(query))
+        self.__url = self.__url.replace("<DATERANGE>", GnewsParser.__DATE_RANGE)
+        if locale == "en-us":
+            self.__url = self.__url.replace("<LOCALE>", GnewsParser.__EN_US_LOCALE)
+        elif locale == "sk":
+            self.__url = self.__url.replace("<LOCALE>", GnewsParser.__SK_SK_LOCALE)
+
+    def __get_clean_query(self, query):
+        return query.replace(" ", "%20")
+
+    def get_results(self):
+        if self.__current_window + timedelta(days=self.__days_step) > self.__end_date:
+            return None
+        time_from = self.__current_window.strftime("%Y-%m-%d")
+        new_url = self.__url
+        new_url = new_url.replace("<AFTER>", time_from)
+        self.__current_window += timedelta(days=self.__days_step)
+        time_to = self.__current_window.strftime("%Y-%m-%d")
+        new_url = new_url.replace("<BEFORE>", time_to)
+
+        res = feedparser.parse(new_url)
+        self.__last_used_url = new_url
+        if res["status"] != 200:
+            print(res["status"])
+            return None
+        return res["entries"]
+
+    def save_state(self, save_file):
+        save = {
+            "last_url": self.__last_used_url,
+            "search_from": self.__start_date.strftime("%Y-%m-%d"),
+            "search_to": self.__end_date.strftime("%Y-%m-%d"),
+            "current_window_date": self.__current_window.strftime("%Y-%m-%d"),
+            "days_step": self.__days_step,
+            "locale": self.__locale,
+            "query": self.__query
+        }
+        with open(save_file, "w") as fp:
+            json.dump(save, fp)
+
+    def setup_search_from_state(self, save_file):
+        f = open(save_file, "r")
+        save = json.load(f)
+        self.__last_used_url = save["last_url"]
+        self.__start_date = datetime.strptime(save["search_from"], "%Y-%m-%d")
+        self.__end_date = datetime.strptime(save["search_to"], "%Y-%m-%d")
+        self.__current_window = datetime.strptime(save["current_window"], "%Y-%m-%d")
+        self.__days_step = save["days_step"]
+        self.__locale = save["locale"]
+        self.__query = save["query"]
+        self.__setup_base_url(self.__query, self.__locale)
---
+++ google_news/google_news/spiders/spider.py
@@ -0,0 +1,74 @@
+import time
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from gnewsparser import GnewsParser
+
+# run with command: scrapy crawl spider -o <outputfile.csv> -t csv
+
+class Spider(scrapy.Spider):
+    name = "spider"
+
+    article_links = {} # storing article links, with crime keywords
+
+    crime_keywords = ['murder']
+
+    custom_settings = {
+        'FEED_FORMAT': 'json',
+        'FEED_URI': 'output.json'
+    }
+
+    def start_requests(self):
+        gnews_parser = GnewsParser()
+
+        # set up searching for each crime defined in crime_keywords
+        for crime_keyword in self.crime_keywords:
+            gnews_parser.setup_search('murder', '2020-12-01', '2020-12-31')
+
+            while True:
+                res = gnews_parser.get_results() # getting articles on daily basis
+
+                if res is None:
+                    break
+
+                for article in res:
+                    # retrieve needed data from Google RSS
+                    link = article['link']
+                    title = article['title']
+                    published = article['published']
+
+                    # make get request on article link
+                    yield scrapy.Request(link,
+                        callback=self.parse,
+                        cb_kwargs=dict(
+                            link=link,
+                            published=published, 
+                            title=title,
+                            crime_keyword=crime_keyword
+                        )
+                    )
+
+                break
+
+    def parse(self, response, link, published, title, crime_keyword):
+        if response.status == 200:
+            try:
+                body_tag = response.css('body').get()
+
+                if link not in self.article_links:
+                    self.article_links[link] = []
+
+                self.article_links[link].append(crime_keyword)
+
+                yield {
+                    "title": title,
+                    "published": published,
+                    "link": link,
+                    "html": body_tag
+                }
+            except:
+                pass
+
+# run Spider
+process = CrawlerProcess()
+process.crawl(Spider)
+process.start()
