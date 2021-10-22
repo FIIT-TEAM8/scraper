d4c190befacb7f3b5ff51034225fa491cac30850
dominik <dom.horvath17@gmail.com>
2021-10-22 23:07:03 +0200

    dumped crimes_map.json + added CrawlerProcess wrapper in main.py

---
+++ google_news/main.py
@@ -0,0 +1,12 @@
+from scrapy.crawler import CrawlerProcess
+from google_news.spiders.spider import Spider
+from scrapy.utils.project import get_project_settings
+import json
+
+process = CrawlerProcess(get_project_settings())
+
+process.crawl(Spider)
+process.start()
+
+with open("../crimes_map.json", "w") as f:
+    json.dump(Spider.article_links, f)
