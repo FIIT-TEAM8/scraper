358bd7849d71a1b72e2c16bd96e1df27371d54df
Dominik Horváth <32178114+Dominik1799@users.noreply.github.com>
2022-03-19 17:06:15 +0100

    Update startServer.sh

--- scraper/startServer.sh
+++ scraper/startServer.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "username = ${SCRAPYD_USERNAME}" >> scrapyd.conf
+echo "password = ${SCRAPYD_PASSWORD}" >> scrapyd.conf
 scrapyd &> output.log &
 scrapyd-deploy
 tail -f output.log
