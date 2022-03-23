da668464059fa7f7ca2408601295ca13d05ef11c
Dominik Horváth <32178114+Dominik1799@users.noreply.github.com>
2022-03-23 09:31:49 +0100

    Update settings.py

--- scraper/google_news/settings.py
+++ scraper/google_news/settings.py
@@ -30,7 +30,7 @@ DOWNLOAD_TIMEOUT = 20
 #     }
 # }
 
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0"
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
 
 # what means integer in ITEM_PIPELINES: https://docs.scrapy.org/en/latest/topics/item-pipeline.html#activating-an-item-pipeline-component
 ITEM_PIPELINES = {
